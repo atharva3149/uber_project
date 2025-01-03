61adc6009056e7325eea68c4b65e38ae31de9eaa
atharva3149 <atharvanalawade3149@gmail.com>
2025-01-03 20:02:36 +0530

    home img

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -4,8 +4,10 @@ const Home = () => {
   return (
     <div>
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-      <div>
-        <img src="" alt="" />
+
+      <div className='h-screen w-screen'>
+        {/* img for temp use */}
+        <img src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
       </div>
     </div>
   )
